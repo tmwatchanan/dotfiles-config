4dd67f9c743973379ed1c7945a88dfa88da758dc
tmwatchanan <tmwatchanan@gmail.com>
2023-05-12 17:14:37 +0700

    fix(sketchybar): language input not showed up

--- sketchybar/items/volume.sh
+++ sketchybar/items/volume.sh
@@ -40,5 +40,3 @@ sketchybar --add slider volume right            \
            --add item volume_icon right         \
            --set volume_icon "${volume_icon[@]}"
 
-sketchybar --add bracket status brew github.bell volume_icon \
-           --set status "${status_bracket[@]}"
