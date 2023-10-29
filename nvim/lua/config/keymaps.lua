cb796c9ae3d7e5a8013c459181d233fa8db23e14
tmwatchanan <tmwatchanan@gmail.com>
2023-10-29 14:22:37 +0700

    chore(nvim/keymaps): add faster resize keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -58,6 +58,10 @@ keymaps.setup = function()
     vim.keymap.set('n', '<C-w>l', '<C-w>>')
     vim.keymap.set('n', '<C-w>k', '<C-w>+')
     vim.keymap.set('n', '<C-w>j', '<C-w>-')
+    vim.keymap.set('n', '<M-,>', '<Cmd>vertical resize -10<CR>', { silent = true })
+    vim.keymap.set('n', '<M-.>', '<Cmd>vertical resize +10<CR>', { silent = true })
+    vim.keymap.set('n', '<M-\'>', '<Cmd>resize +10<CR>', { silent = true })
+    vim.keymap.set('n', '<M-/>', '<Cmd>resize -10<CR>', { silent = true })
 
     -- INFO: remap jump keys
     vim.keymap.set('n', '<M-o>', '<C-i>') -- <C-i> is <Tab>, so we need to replace it with another
