c35e4561d82f9f3b6e8531c3996627485130d8a2
tmwatchanan <tmwatchanan@gmail.com>
2023-10-29 15:57:46 +0700

    feat(nvim/swenv): notify when python interpreter is changed

--- nvim/lua/plugins/swenv-config.lua
+++ nvim/lua/plugins/swenv-config.lua
@@ -7,6 +7,8 @@ M.opts = {
     post_set_venv = function()
         local python_path = require('config.python').get_python_path()
         vim.cmd('PyrightSetPythonPath ' .. python_path)
+        local venv = require('swenv.api').get_current_venv()
+        vim.notify(('󰌠 Python interpreter changed (%s)'):format(venv.name), vim.log.levels.INFO)
     end,
 }
 
