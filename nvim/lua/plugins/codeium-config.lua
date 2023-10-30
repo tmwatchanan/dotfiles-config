c647ebeead4ccb46aca0242d6317e94f86ee3dd6
tmwatchanan <tmwatchanan@gmail.com>
2023-10-30 23:30:28 +0700

    fix(nvim/codeium.vim): accept the suggestion to input

--- nvim/lua/plugins/codeium-config.lua
+++ nvim/lua/plugins/codeium-config.lua
@@ -6,13 +6,14 @@ local M = {
 M.keys = function()
     local codeium_keymap = require('config.keymaps').codeium
     return {
-        { codeium_keymap.accept,   function() vim.fn['codeium#Accept']() end,             mode = 'i', { expr = true } },
+        { codeium_keymap.accept,   function() vim.api.nvim_input(vim.fn['codeium#Accept']()) end,             mode = 'i', { expr = true } },
         { codeium_keymap.previous, function() vim.fn['codeium#CycleCompletions'](-1) end, mode = 'i', { expr = true } },
         { codeium_keymap.next,     function() vim.fn['codeium#CycleCompletions'](1) end,  mode = 'i', { expr = true } },
         { codeium_keymap.clear,    function() vim.fn['codeium#Clear']() end,              mode = 'i', { expr = true } },
     }
 end
 
+vim.g.codeium_tab_fallback = "<Tab>"
 -- vim.g.codeium_disable_bindings = true
 -- vim.g.codeium_no_map_tab = true
 
