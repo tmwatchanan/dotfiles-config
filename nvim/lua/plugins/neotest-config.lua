3696b2a9c4f2e5c80b2e401e566c3a5469fdc74a
tmwatchanan <tmwatchanan@gmail.com>
2023-11-01 00:42:24 +0700

    chore(nvim/neotest): no longer needed `FixCursorHold.nvim` since neovim/neovim#20198

--- nvim/lua/plugins/neotest-config.lua
+++ nvim/lua/plugins/neotest-config.lua
@@ -3,7 +3,6 @@ local M = {
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-treesitter/nvim-treesitter',
-        'antoinemadec/FixCursorHold.nvim',
         'nvim-neotest/neotest-python',
         'nvim-neotest/neotest-plenary',
         'nvim-neotest/neotest-vim-test',
