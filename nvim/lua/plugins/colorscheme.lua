50ebdfffe06b703450af88ec7bce6a17f14376d8
tmwatchanan <tmwatchanan@gmail.com>
2023-12-30 12:37:26 +0700

    refactor(nvim/colorscheme/kanagawa): disable underline for `LspReferenceWrite`

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -117,8 +117,8 @@ M.config = function()
                 -- MarkVirtTextHL = { fg = M.colorset.bright_blue, bg = M.colorset.transparent, nocombine = true },
             }
 
-            local local_highlight = {
-                LocalHighlight = { bg = M.colorset.bg1, bold = true, nocombine = true },
+            local lsp_highlight = {
+                LspReferenceWrite = { bg = theme.diff.text, underline = false },
             }
 
             local noice_highlight = {
@@ -163,7 +163,7 @@ M.config = function()
             overrideHighlightConfig(telescope_highlight)
             overrideHighlightConfig(flit_highlight)
             overrideHighlightConfig(marks_highlight)
-            overrideHighlightConfig(local_highlight)
+            overrideHighlightConfig(lsp_highlight)
             overrideHighlightConfig(noice_highlight)
             overrideHighlightConfig(incline_highlight)
             overrideHighlightConfig({
