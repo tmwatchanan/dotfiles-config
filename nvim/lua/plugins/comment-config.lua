7439aef377fc1369f6edb17baa5439d542764d1e
tmwatchanan <tmwatchanan@gmail.com>
2024-02-21 21:45:38 +0700

    feat(nvim/comment): replace `lucastavaresa/SingleComment.nvim` with `numToStr/Comment.nvim`
    for more textobjects and motions

--- nvim/lua/plugins/comment-config.lua
+++ nvim/lua/plugins/comment-config.lua
@@ -1,20 +1,15 @@
 local M = {
-    'lucastavaresa/SingleComment.nvim',
+    'numToStr/Comment.nvim',
     dependencies = {
         { 'nvim-treesitter' },
         {
             'JoosepAlviste/nvim-ts-context-commentstring',
             opts = { enable_autocmd = false },
         },
-    }
-}
-
-M.keys = {
-    { mode = { 'n' },      'gcc', function() return require('SingleComment').SingleComment() end, expr = true },
-    { mode = { 'v' },      'gcc', function() require('SingleComment').Comment() end },
-    { mode = { 'n' },      'gca', function() require('SingleComment').ToggleCommentAhead() end },
-    { mode = { 'n' },      'gcA', function() require('SingleComment').CommentAhead() end },
-    { mode = { 'n', 'v' }, 'gcb', function() require('SingleComment').BlockComment() end },
+    },
+    opts = {
+    },
+    lazy = false,
 }
 
 return M
