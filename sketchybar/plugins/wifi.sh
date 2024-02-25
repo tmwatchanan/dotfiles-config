a9e8ae50744f54ac707ce50a5782df57641bbb65
tmwatchanan <tmwatchanan@gmail.com>
2024-02-25 19:26:04 +0700

    feat: update `sketchybar` and `skhd` configs

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,12 +3,12 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 if [ "$SENDER" = "wifi_change" ]; then
-    INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
     if [[ -z "$INFO" ]]; then
         ICON="󰤫"
     else
         ICON="󰤨"
     fi
+
     WIFI=${INFO:-"Not Connected"}
 
     sketchybar --set $NAME label="${WIFI}" icon="${ICON}"
