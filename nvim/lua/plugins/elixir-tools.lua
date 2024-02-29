8fb4e7de968df977f5196a5e780a5eb0361bb0d9
tmwatchanan <tmwatchanan@gmail.com>
2024-02-29 22:38:42 +0700

    chore(nvim/exlixir): store `elixir-tools.nvim`

---
+++ nvim/lua/plugins/elixir-tools.lua
@@ -0,0 +1,35 @@
+-- local M = {
+--     "elixir-tools/elixir-tools.nvim",
+--     version = "*",
+--     event = { "BufReadPre", "BufNewFile" },
+--     dependencies = {
+--         "nvim-lua/plenary.nvim",
+--     },
+-- }
+
+-- M.opts = function()
+--     -- local elixir = require("elixir")
+--     local elixirls = require("elixir.elixirls")
+
+--     return {
+--         elixirls = {
+--             enable = true,
+--             settings = elixirls.settings {
+--                 dialyzerEnabled = false,
+--                 enableTestLenses = false,
+--             },
+--         }
+--     }
+-- end
+
+-- M.keys = function()
+--     local keymap = require('config.keymaps').elixir_tools
+--     return {
+--         { keymap.elixir_from_pipe,    '<Cmd>ElixirFromPipe<CR>',    buffer = true, noremap = true },
+--         { keymap.elixir_to_pipe,      '<Cmd>ElixirToPipe<CR>',      buffer = true, noremap = true },
+--         { keymap.elixir_expand_macro, '<Cmd>ElixirExpandMacro<CR>', mode = 'v',    buffer = true, noremap = true },
+--     }
+-- end
+
+-- return M
+return {}
