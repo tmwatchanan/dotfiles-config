e7bddf5f8df55776284284feef9fe5571c3d36d6
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-03-09 22:05:30 +0700

    feat(nvim/treesitter): add `# python` injection query for pyspark embedding in string

--- nvim/after/queries/python/injections.scm
+++ nvim/after/queries/python/injections.scm
@@ -1,18 +1,25 @@
-; extends
+;; extends
 
-(expression_statement
-  (call
-    function: (attribute
-      object: (identifier) @_spark_object (#eq? @_spark_object "spark_session")
-      attribute: (identifier) @_run_attribute (#eq? @_run_attribute "run"))
-    arguments: (argument_list
-      (call
-        function: (attribute
-          object: (identifier)
-          attribute: (identifier))
-        arguments: (argument_list
-          (string
-            (string_content) @injection.content)))))
-  (#set! injection.language "python")
-) @_spark_string
+; (expression_statement
+;   (call
+;     function: (attribute
+;       object: (identifier) @_spark_object (#match? @_spark_object ".*spark.*")
+;       attribute: (identifier) @_run_attribute (#eq? @_run_attribute "run"))
+;     arguments: (argument_list
+;       (call
+;         function: (attribute
+;           object: (identifier)
+;           attribute: (identifier))
+;         arguments: (argument_list
+;           (string
+;             (string_content) @injection.content)))))
+;   (#set! injection.language "python")
+; ) @_spark_string
+;
+
+(string
+  (string_content) @injection.content
+    (#lua-match? @injection.content "^# *python")
+    (#set! injection.language "python")
+) @_python_string_injection
 
