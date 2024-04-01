643e7d6c4d4c61176ede26b9513d482840fec417
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-04-01 19:47:54 +0700

    feat(nvim/focus): allow multiple different fixed widths for filetypes

--- nvim/lua/plugins/focus-config.lua
+++ nvim/lua/plugins/focus-config.lua
@@ -5,14 +5,19 @@ local M = {
 
 M.init = function()
     local ignore_buftypes = { 'nofile', 'prompt', 'popup', 'terminal' }
-    local ignore_filetypes = { 'undotree' }
-    local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+    local ignore_extension_filetypes = { 'undotree', 'neotest-summary' }
+    local dapui_filetypes = { 'dapui_breakpoints', 'dap-repl', 'dapui_console', 'dapui_scopes', 'dapui_watches', 'dapui_stacks' }
+    local filetype_widths = {
+        ['undotree'] = 40,
+        ['neotest-summary'] = 40,
+    }
+    local ignore_filetypes = vim.tbl_extend('keep', ignore_extension_filetypes, dapui_filetypes)
 
+    local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
     vim.api.nvim_create_autocmd('WinEnter', {
         group = augroup,
-        callback = function(_)
-            if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
-            then
+        callback = function()
+            if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
                 vim.w.focus_disable = true
             else
                 vim.w.focus_disable = false
@@ -20,28 +25,27 @@ M.init = function()
         end,
         desc = 'Disable focus autoresize for BufType',
     })
-
+    local fixed_width_augroup = vim.api.nvim_create_augroup('FocusFixedWidth', { clear = true })
     vim.api.nvim_create_autocmd('FileType', {
-        group = augroup,
+        group = fixed_width_augroup,
         callback = function()
             if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-                vim.api.nvim_win_set_width(0, 40)
-                vim.b.focus_disable = true
-            end
-        end,
-        desc = 'Disable focus autoresize for FileType',
-    })
-
-    local dapui_filetypes = { 'dapui_breakpoints', 'dap-repl', 'dapui_console', 'dapui_scopes', 'dapui_watches', 'dapui_stacks' }
-    local focus_keymaps = require('config.keymaps').focus
-    vim.api.nvim_create_autocmd('FileType', {
-        group = augroup,
-        callback = function()
-            if vim.tbl_contains(dapui_filetypes, vim.bo.filetype) then
+                local focus_keymaps = require('config.keymaps').focus
                 vim.keymap.set('n', focus_keymaps.split_left, '<C-w><C-h>', { buffer = true, desc = "focus left" })
                 vim.keymap.set('n', focus_keymaps.split_right, '<C-w><C-l>', { buffer = true, desc = "focus right" })
                 vim.keymap.set('n', focus_keymaps.split_up, '<C-w><C-k>', { buffer = true, desc = "focus up" })
                 vim.keymap.set('n', focus_keymaps.split_down, '<C-w><C-j>', { buffer = true, desc = "focus down" })
+                vim.w.focus_disable = true
+            else
+                vim.w.focus_disable = false
+            end
+
+            local width = filetype_widths[vim.bo.filetype]
+            vim.print(vim.bo.filetype)
+            if width then
+                vim.print(("set %s width -> %d"):format(vim.bo.filetype, width))
+                vim.api.nvim_win_set_width(0, width)
+                vim.b.focus_disable = true
             end
         end,
         desc = 'Disable focus autoresize for FileType',
