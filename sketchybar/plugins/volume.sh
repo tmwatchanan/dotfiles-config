c0a61d0eba3df7082f2ab9bd5429507bb1e76b11
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-04-06 23:15:24 +0700

    chore(sketchybar): remove unused plugins

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -1,56 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-WIDTH=100
+VOLUME=$INFO
 
-volume_change() {
-  source "$HOME/.config/sketchybar/icons.sh"
-  case $INFO in
-    [6-9][0-9]|100) ICON=$VOLUME_100
-    ;;
-    [3-5][0-9]) ICON=$VOLUME_66
-    ;;
-    [1-2][0-9]) ICON=$VOLUME_33
-    ;;
-    [1-9]) ICON=$VOLUME_10
-    ;;
-    0) ICON=$VOLUME_0
-    ;;
-    *) ICON=$VOLUME_100
-  esac
-
-  sketchybar --set volume_icon label=$ICON
-
-  sketchybar --set $NAME slider.percentage=$INFO \
-             --animate tanh 30 --set $NAME slider.width=$WIDTH 
-
-  sleep 2
-
-  # Check wether the volume was changed another time while sleeping
-  FINAL_PERCENTAGE=$(sketchybar --query $NAME | jq -r ".slider.percentage")
-  if [ "$FINAL_PERCENTAGE" -eq "$INFO" ]; then
-    sketchybar --animate tanh 30 --set $NAME slider.width=0
-  fi
-}
-
-mouse_clicked() {
-  osascript -e "set volume output volume $PERCENTAGE"
-}
-
-mouse_entered() {
-  sketchybar --set $NAME slider.knob.drawing=on
-}
-
-mouse_exited() {
-  sketchybar --set $NAME slider.knob.drawing=off
-}
-
-case "$SENDER" in
-  "volume_change") volume_change
+case $VOLUME in
+  [6-9][0-9]|100)
+    ICON="墳"
+    ICON_PADDING_RIGHT=5
   ;;
-  "mouse.clicked") mouse_clicked
+  [3-5][0-9])
+    ICON="奔"
+    ICON_PADDING_RIGHT=8
   ;;
-  "mouse.entered") mouse_entered
+  [1-2][0-9]) 
+    ICON=""
+    ICON_PADDING_RIGHT=11
   ;;
-  "mouse.exited") mouse_exited
+  [1-9])
+    ICON=""
+    ICON_PADDING_RIGHT=18
   ;;
+  *) 
+    ICON="婢"
+    ICON_PADDING_RIGHT=12
 esac
+
+sketchybar --set $NAME icon="$ICON" icon.padding_right=$ICON_PADDING_RIGHT label="$VOLUME%"
