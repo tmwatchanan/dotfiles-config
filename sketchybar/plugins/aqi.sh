678d9bfb9ddc455b8eb64ee94c0f343a5b8e63b3
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-04-06 23:15:03 +0700

    fix(sketchybar/aqi): mismatched constant, combine printf statements

--- sketchybar/plugins/aqi.sh
+++ sketchybar/plugins/aqi.sh
@@ -4,17 +4,14 @@ location_json=$(curl -s https://ipinfo.io/json)
 location=$(echo $location_json | jq -r '.loc')
 latitude=$(echo $location | cut -d',' -f1)
 longitude=$(echo $location | cut -d',' -f2)
-LIMIT_KM=2
-aqi_json=$(curl -s "https://www-old.cmuccdc.org/api2/dustboy/near/$latitude/$longitude/$limit_km")
+LIMIT_KM=5
+aqi_json=$(curl -s "https://www-old.cmuccdc.org/api2/dustboy/near/$latitude/$longitude/$LIMIT_KM")
 first_station_data=$(echo $aqi_json | jq '.[0]')
 
 pm25=$(echo $first_station_data | jq -r '.pm25_us_aqi' || echo "-")
 
 color=$(echo $first_station_data | jq -r '.us_color' || echo "255,255,255")
-IFS=',' read -ra rgb <<< "$color"
-red=$(printf "%02X" "${rgb[0]}")
-green=$(printf "%02X" "${rgb[1]}")
-blue=$(printf "%02X" "${rgb[2]}")
-icon_color="0xff$red$green$blue"
+IFS=',' read -r red green blue <<< $(echo "$color" | tr -d '[:space:]')
+icon_color=$(printf "0x%02X%02X%02X%02X" 255 "$red" "$green" "$blue")
 
 sketchybar --set $NAME label="$pm25" icon.color=${icon_color}
