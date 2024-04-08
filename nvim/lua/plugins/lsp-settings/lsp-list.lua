909614adeaa1b2c14422ef70fd85732a6f6c188a
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-04-08 10:31:36 +0700

    chore(nvim/lsp): clean up unused LSPs

--- nvim/lua/plugins/lsp-settings/lsp-list.lua
+++ nvim/lua/plugins/lsp-settings/lsp-list.lua
@@ -1,13 +1,10 @@
 return {
-    clangd = require('plugins.lsp-settings.clangd'),
-    jsonls = {},
     lua_ls = require('plugins.lsp-settings.lua-ls'),
     marksman = {},
-    volar = require('plugins.lsp-settings.volar'),
     basedpyright = require('plugins.lsp-settings.basedpyright'),
     ruff_lsp = require('plugins.lsp-settings.ruff-lsp'),
-    rust_analyzer = {},
     helm_ls = {},
     yamlls = {},
+    jsonls = {},
     julials = {},
 }
