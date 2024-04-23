0e65d9e846398621255b6a812ede288070c459f3
momeppkt <peeranut32@gmail.com>
2024-04-23 14:13:21 +0700

    fix(nvim/lsp): fix `vim.lsp.inlay_hint.enable()` interface changed

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -114,12 +114,12 @@ lsp_setup_module.config = function()
             vim.api.nvim_create_autocmd('InsertEnter', {
                 buffer = bufnr,
                 group = inlayhint_augroup,
-                callback = function() vim.lsp.inlay_hint.enable(bufnr, true) end,
+                callback = function() vim.lsp.inlay_hint.enable(true, { bufnr = bufnr }) end,
             })
             vim.api.nvim_create_autocmd('InsertLeave', {
                 buffer = bufnr,
                 group = inlayhint_augroup,
-                callback = function() vim.lsp.inlay_hint.enable(bufnr, false) end,
+                callback = function() vim.lsp.inlay_hint.enable(false, { bufnr = bufnr }) end,
             })
 
             vim.notify_once('inlayhint enabled', vim.log.levels.INFO, { title = client.name .. ':' })
