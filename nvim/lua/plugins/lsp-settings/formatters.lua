4ff9f52240317e90831e3f456e840e404c3dc26e
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-08-30 01:38:33 +0700

    feat(nvim/none-ls): add formatter and diagnostics for fish shell

--- nvim/lua/plugins/lsp-settings/formatters.lua
+++ nvim/lua/plugins/lsp-settings/formatters.lua
@@ -7,6 +7,10 @@ return {
     none_ls = {
         sources = {
             require('null-ls').builtins.formatting.prettierd,
+
+            require('null-ls').builtins.formatting.shfmt,
+            require('null-ls').builtins.diagnostics.fish,
+            require('null-ls').builtins.formatting.fish_indent,
         },
     },
 }
