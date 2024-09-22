8e2cb1138fdd24897d55b5a961189c235a33902a
momeppkt <peeranut32@gmail.com>
2024-09-22 14:47:47 +0700

    fix(sketchybar/wifi): fix the string manipulation for `system_profiler` output

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,8 +3,19 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 if [ "$SENDER" = "wifi_change" ]; then
-    airport_info=$(/usr/sbin/system_profiler SPAirPortDataType)
-    INFO=$(echo "$airport_info" | awk '/Current Network Information:/{f=1; next} f && /^[[:space:]]*[A-Za-z0-9\-_]+:/{print $1; exit}' | sed 's/://')
+    INFO=$(
+        system_profiler SPAirPortDataType -detailLevel basic |
+        awk '/Current Network Information:/ {
+            getline
+            if ($0 ~ /^[[:space:]]*$/) {
+                next
+            }
+            gsub(/^[[:space:]]+|:$/, "", $0)
+            print $0
+            exit
+        }'
+    )
+
     if [[ -z "$INFO" ]] || [[ -z "${INFO##You are not*}" ]]; then
         ICON="󰤫"
         WIFI="Not Connected"
