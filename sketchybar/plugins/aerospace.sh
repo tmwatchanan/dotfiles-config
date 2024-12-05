3e1cf5273cf549bc5411102c5324ecbef407ada5
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-12-06 00:01:43 +0700

    fix(sketchybar/aerospace): workspace for each monitor, add icons

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -2,9 +2,11 @@
 
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+#
+source "$CONFIG_DIR/colors.sh"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME background.drawing=on icon.color=$WHITE
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME background.drawing=off icon.color=$GREY
 fi
