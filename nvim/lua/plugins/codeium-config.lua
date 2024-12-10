71abb6a7a048ec44083610ee4c00097a0588ab37
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-12-10 13:00:50 +0700

    feat(nvim/codeium): replace `Exafunction/codeium.vim` with `monkoose/neocodeium`

--- nvim/lua/plugins/codeium-config.lua
+++ nvim/lua/plugins/codeium-config.lua
@@ -1,33 +1,23 @@
--- local M =
--- {
---     'monkoose/neocodeium',
---     event = 'VeryLazy',
---     config = function()
---         local neocodeium = require('neocodeium')
---         neocodeium.setup()
---         vim.keymap.set('i', '<A-f>', neocodeium.accept)
---     end,
---     cond = not vim.g.vscode,
--- }
 local M = {
-    'Exafunction/codeium.vim',
-    event = { 'BufEnter' },
-    -- init = function()
-    --     vim.g.codeium_tab_fallback = "<Tab>"
-    --     vim.g.codeium_disable_bindings = true
-    --     vim.g.codeium_no_map_tab = true
-    -- end,
+    'monkoose/neocodeium',
+    event = 'VeryLazy',
     cond = not vim.g.vscode,
 }
 
+M.config = function()
+    local neocodeium = require('neocodeium')
+    neocodeium.setup()
+end
+
 M.keys = function()
+    local neocodeium = require('neocodeium')
     local codeium_keymap = require('config.keymaps').codeium
     return {
-        { codeium_keymap.accept,   function() vim.api.nvim_input(vim.fn['codeium#Accept']()) end, mode = 'i', expr = true },
-        { codeium_keymap.previous, function() vim.fn['codeium#CycleCompletions'](-1) end,         mode = 'i', expr = true },
-        { codeium_keymap.next,     function() vim.fn['codeium#CycleCompletions'](1) end,          mode = 'i', expr = true },
-        { codeium_keymap.clear,    function() vim.fn['codeium#Clear']() end,                      mode = 'i', expr = true },
-        { codeium_keymap.chat,     function() vim.fn['codeium#Chat']() end,                       mode = 'n' },
+        { codeium_keymap.accept,   function() neocodeium.accept() end,        mode = 'i', expr = true },
+        { codeium_keymap.previous, function() neocodeium.cycle(-1) end,       mode = 'i', expr = true },
+        { codeium_keymap.next,     function() neocodeium.cycle(1) end,        mode = 'i', expr = true },
+        { codeium_keymap.clear,    function() neocodeium.clear() end,         mode = 'i', expr = true },
+        { codeium_keymap.chat,     function() vim.cmd('NeoCodeium chat') end, mode = 'n' },
     }
 end
 
