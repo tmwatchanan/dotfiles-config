90c04f93a909823232b9535e0322e2e807d00530
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-12-31 16:01:06 +0700

    fix(nvim/lsp/ruff): fix deprecated lspconfig util function, use default instead

--- nvim/lua/plugins/lsp-settings/ruff.lua
+++ nvim/lua/plugins/lsp-settings/ruff.lua
@@ -1,16 +1,8 @@
-local util = require('lspconfig.util')
-
-local root_files = {
-    'pyproject.toml',
-    'ruff.toml',
-}
-
 return {
     on_attach = function(client, _)
         -- Disable hover in favor of Pyright
         client.server_capabilities.hoverProvider = false
     end,
-    root_dir = util.root_pattern(unpack(root_files)) or util.find_git_ancestor,
     init_options = {
         settings = {
             lineLength = 88,
