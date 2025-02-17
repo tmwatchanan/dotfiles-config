9e26f8712e6ee517b54f25b31b0df66f97895263
tmwatchanan <tmwatchanan@users.noreply.github.com>
2025-02-17 09:27:29 +0700

    feat(nvim/lsp): remove `helm_ls` and `julials` from default lsp list

--- nvim/lua/plugins/lsp-settings/lsp-list.lua
+++ nvim/lua/plugins/lsp-settings/lsp-list.lua
@@ -3,8 +3,6 @@ return {
     marksman = {},
     basedpyright = require('plugins.lsp-settings.basedpyright'),
     ruff = require('plugins.lsp-settings.ruff'),
-    helm_ls = {},
     yamlls = {},
     jsonls = {},
-    julials = {},
 }
