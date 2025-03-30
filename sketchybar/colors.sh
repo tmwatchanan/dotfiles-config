92fb2e0f29e2fbe1cc600d0242b2eb6ccab68d38
momeppkt <peeranut32@gmail.com>
2025-03-30 17:03:28 +0700

    feat(sketchybar): update config to matched with notch, add click script to menubar

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -14,7 +14,7 @@ export GREY=0xff939ab7
 export TRANSPARENT=0x00000000
 
 # General bar colors
-export BAR_COLOR=0xd5262626
+export BAR_COLOR=0xff000000
 export BAR_BORDER_COLOR=0xbb505050
 export ICON_COLOR=$WHITE
 export LABEL_COLOR=$WHITE
