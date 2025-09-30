38516e649d46b180e1ed60f5375093d49e19a28f
momeppkt <peeranut32@gmail.com>
2025-09-30 14:37:04 +0700

    chore(installer): update list of dependencies tools

--- config-installer.sh
+++ config-installer.sh
@@ -2,7 +2,7 @@
 
 # WARN: must have brew installed
 # brew install wget luarocks neovim lazygit git-flow-avh git-delta ripgrep fd jq eza
-brew install wget lazygit git-flow-avh git-delta ripgrep fd eza fnm neovim nushell gh bat pyenv tmux yabai skhd sketchybar starship
+brew install wget lazygit git-flow-avh git-delta ripgrep fd eza fnm neovim nushell gh bat pyenv tmux starship aerospace tree-sitter-cli carapace
 
 # INFO: -- move nvim config files to local config directory
 config_path=~/.config
