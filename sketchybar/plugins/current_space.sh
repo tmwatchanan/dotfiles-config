ce7e2e165c67de5d33b9f22d2ed365aeb57246d0
momeppkt <peeranut32@gmail.com>
2026-03-16 16:54:55 +0700

    chore: clean up unused config files

--- sketchybar/plugins/current_space.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-ICON_PADDING=8
-
-sketchybar --set $NAME                              \
-                background.drawing=$SELECTED        \
-                icon.padding_left=$ICON_PADDING     \
-                icon.padding_right=$ICON_PADDING    
-
